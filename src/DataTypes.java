8685949993d6fa18b5f9889c6358052e148db883
AmaliaDwiOktavia <AmaliaDwiOktavia@gmail.com>
2021-06-08 20:23:17 +0800

    deklarasi variabel dan tipe data

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,9 +1,10 @@
 public class DataTypes {
     public static void main(String[] args) {
 
-        String namaDepan = "Yuniati";
-        String namaBelakang = "Rafiah";
-        int usia = 20;
+        String namaDepan = "Amalia";
+        String namaTengah = "Dwi";
+        String namaBelakang = "Oktavia";
+        int usia = 21;
         int targetTahunKuliah = 4;
         double ipk = 3.389764512;
         char nilaiAbjad = 'A';
