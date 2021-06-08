08632ff81c48a018e6129232a1cbfb205ac135bd
AmaliaDwiOktavia <AmaliaDwiOktavia@gmail.com>
2021-06-08 20:35:39 +0800

    Project IDEA

---
+++ Modul01_IDEA/src/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main (String[] args ) {
+        String namaDepan = "Amalaia";
+        String namaTengah = "Dwi";
+        String namaBelakang = "Oktavia";
+        int usia = 21;
+        int targetTahunKuliah = 4;
+        double ipk = 3.89764512;
+        char nilaiAbjad = 'A';
+        boolean cantik = true;
+
+        System.out.println(namaDepan + namaTengah + namaBelakang);
+    }
+}
