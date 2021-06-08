edb476be2c865f830f89b5a03baf6f835c783ad2
AmaliaDwiOktavia <AmaliaDwiOktavia@gmail.com>
2021-06-08 20:39:46 +0800

    Operasi Tipe String

--- Modul01_IDEA/src/Main.java
+++ Modul01_IDEA/src/Main.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 
 public class Main {
     public static void main (String[] args ) {
-        String namaDepan = "Amalaia";
+        String namaDepan = "Amalia";
         String namaTengah = "Dwi";
         String namaBelakang = "Oktavia";
         int usia = 21;
@@ -12,5 +12,24 @@ public class Main {
         boolean cantik = true;
 
         System.out.println(namaDepan + namaTengah + namaBelakang);
+        char[] uniskaChar = {'U', 'N', 'I', 'S', 'K', 'A'};
+        String uniskaString = new String(uniskaChar);
+        System.out.println(uniskaString);
+
+        String namaLengkap = namaDepan + " " + namaTengah + " " + namaBelakang;
+        System.out.println(namaLengkap.length());
+        System.out.println(namaLengkap.indexOf("NIS"));
+        System.out.println(namaLengkap.substring(5));
+        System.out.println(namaLengkap.substring(5, 12));
+        System.out.println(namaLengkap.replace("Ama", "Kar"));
+        System.out.println(namaLengkap.toUpperCase());
+        System.out.println(namaLengkap.toLowerCase());
+        System.out.println(namaLengkap.charAt(6));
+
+        String[] namaArray = namaLengkap.split(" ");
+
+        for (String nama : namaArray) {
+            System.out.println(nama);
+        }
     }
 }
